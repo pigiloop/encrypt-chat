898b5ad610034502a267c05d5b5b590947bd0317
Konstantin <pigiloop@mail.ru>
2025-11-15 21:12:17 +1100

    Commit to Jenkins with TestCoverage add files

---
+++ backend/src/main/java/ru/vinhome/controller/UserRestController.java
@@ -0,0 +1,64 @@
+package ru.vinhome.controller;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import ru.vinhome.model.User;
+import ru.vinhome.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@Path("/v1/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserRestController {
+
+    private static final UserServiceImpl userService = new UserServiceImpl();
+
+    @POST
+    public Response save(final User user) {
+
+        if (user.getUserName().isBlank() || user.getEmail().isBlank() || user.getPassword().isBlank()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("User is empty, set data in user")
+                    .build();
+        }
+        try {
+            if (userService.emailExist(user.getEmail())) {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("This email exist, please enter other email")
+                        .build();
+            }
+            userService.save(user);
+
+            return Response.status(Response.Status.CREATED)
+                    .entity(user)
+                    .build();
+        } catch (SQLException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id") final long id) {
+        try {
+            try {
+                return Response.ok()
+                        .entity(userService.findById(id))
+                        .build();
+            } catch (SQLException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (NullPointerException e) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("e.getMessage()")
+                    .build();
+        }
+    }
+
+
+}
